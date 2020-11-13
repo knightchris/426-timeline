9152251839855d692e455de56a8d09ec61b75994
Chris Knight <chknight@live.unc.edu>
2020-11-13 16:12:58 -0500

    pre-heroku test deploy

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,4 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   return res.status(200).json({ msg: 'Welcome to the timeline api'});
 });
+
+router.get('/welcome', function(req,res, next) {
+  return res.status(200).json({msg: 'hello'});
+});
+
 module.exports = router;
