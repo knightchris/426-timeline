45866fc69b2b68ff9f99bfc0347b92749ad7001c
Chris Knight <chknight@live.unc.edu>
2020-11-13 18:22:48 -0500

    api serving database requests in dev env

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:ilovecake@localhost:5432/timelinedb',
+  ssl: process.env.DATABASE_URL ? true : false
+});
+
+
 
 router.get('/', function(req, res, next) {
   return res.status(200).json({ msg: 'Welcome to the timeline api'});
 });
 
-router.get('/welcome', function(req,res, next) {
-  return res.status(200).json({msg: 'hello'});
+router.get('/users', function(req,res, next) {
+  client.connect();
+
+  client.query('SELECT * FROM Users;', (err, results) => {
+    if (err) throw err;
+    return res.status(200).json(results.rows);
+  
+});
+
 });
 
 module.exports = router;
