138250c0cd3a2e03bc526756b09d0a1cc5b5bae7
Chris Knight <chknight@live.unc.edu>
2020-11-15 02:18:15 -0500

    createuser endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -37,12 +37,54 @@ router.post('/logout', function(req, res) {
   res.status(200).json(true);
 });
 
-router.get('/testlogin', function(req, res) {
+router.get('/checklogin', function(req, res) {
   if (req.session.user == undefined) {
     res.status(403).send("You are not logged in");
   } else {
     res.status(200).send("You are logged in");
   }
+});
+
+router.post('/createuser', async function(req, res) {
+  let user = req.body.username;
+  let password = req.body.password;
+
+  if (password.length < 5 || user.length < 2) {
+    return res.status(406).send("Password must be at least 5 characters, user must be at least 2 characters")
+  }
+  
+  let sql = 'SELECT username FROM Users WHERE username=$1;';
+  let values = [user];
+  let result = await pool.query(sql, values);
+  if (result.rows[0]) {
+    return res.status(404).send("User already exists");
+  } else {
+    sql = "INSERT INTO Users values ($1, $2, false)";
+    values.push(password);
+    result = await pool.query(sql, values);
+    res.status(200).send("User successfully created")
+  }
+
+  /*
+  pool.query(sql, values, (err, result) => {
+    if (result.rows[0]) {
+      this.continueInsert = false;
+      res.status(404).send("User already exists");
+    }
+  })
+
+  console.log(continueInsert);
+
+  if (!continueInsert) {
+    sql = "INSERT INTO Users values ($1, $2, false)"
+    pool.query(sql, values, (err,result) => {
+      res.status(200).send("User successfully created")
+    })
+  }
+  */
+  
+
+  
 });
 
 
