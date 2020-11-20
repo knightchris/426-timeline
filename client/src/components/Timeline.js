e112e3d859a99d693f0d01c8e1ec0c111ea735fc
Anthony Michaels <Ajamesmichaels@gmail.com>
2020-11-19 19:35:09 -0500

    Attempts to add axios request to timeline

--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -1,21 +1,57 @@
-import { render } from 'react-dom';
+import React from 'react';
+import axios from 'axios'
 import '../css/Timeline.css';
 import TimelineItem from './TimelineItem.js'
 
-function Timeline(props) {
+class Timeline extends React.Component {
     // let {cards, sortBy, filterBy} = props
-    let cards = [{},{},{}];
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            cards: []
+        };
+    }
+
+
+    async componentDidMount() {
+        const result = await axios({
+            method: 'post',
+            url: 'http://localhost:3000/mediacards',
+            withCredentials: true,
+            data: {
+                "approved": true
+            }
+        });
+        console.log(result);
+        this.setState({
+            isLoaded: true,
+            cards: result.data
+        });
+    }
+
+
     // handle sorting
 
     // handle filtering
 
-    const timelineItems = cards.map(card => <TimelineItem key={card.id} data={card}></TimelineItem>)
 
-    return (
-        <div className="timeline">
-            {timelineItems}
-        </div>
-    )
+
+    render() {
+        const { error, isLoaded, cards } = this.state;
+        const timelineItems = cards.map(card => <TimelineItem key={card.id} data={card}></TimelineItem>)
+        if (error) {
+            return <div className="timeline">Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div className="timeline">Loading...</div>;
+        } else {
+            <div className="timeline">
+                {timelineItems}
+            </div>
+        }
+    }
+
 }
 
 export default Timeline;
