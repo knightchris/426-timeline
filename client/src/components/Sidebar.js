a203f00f848e70ac9601522f18c86107f2478de4
shercami <shercami@live.unc.edu>
2020-11-22 20:14:09 -0500

    sort css changes

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -35,7 +35,7 @@ function Sidebar(props) {
 
   async function handleSort(event) {
     
-    let choice = document.querySelector("#dropdown").value;
+    let choice = event.target.textContent;
     let sort= [];
     if (choice == "Published Date, Ascending") {
       sort = ['pubdate', 'asc'];
@@ -79,17 +79,14 @@ function Sidebar(props) {
          <Autocomplete
             options={props.cards}
           /> 
-            <div className="custom-select">
-              <select name="dropdown" id="dropdown">
-                <option className="sort-option">Sort by:</option>
-                <option className="sort-option" value="Published Date, Ascending">Published Date, Ascending</option>
-                <option className="sort-option" value="Published Date, Descending">Published Date, Descending</option>
-                <option className="sort-option" value="Universe Date, Ascending">Universe Date, Ascending</option>
-                <option className="sort-option" value="Universe Date, Descending">Universe Date, Descending</option>
-              </select>
-              <svg onClick={handleSort} id="sort-go" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right" fill="white" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-              </svg>
+            <div className="custom-select" id="dropdown">
+              <button className="sidenav-button" id="dropdownbtn">Sort</button>
+              <ul className="dropdown-content">
+                <li onClick={handleSort} className="sort-option" >Published Date, Ascending</li>
+                <li onClick={handleSort} className="sort-option" >Published Date, Descending</li>
+                <li onClick={handleSort} className="sort-option" >Universe Date, Ascending</li>
+                <li onClick={handleSort} className="sort-option" >Universe Date, Descending</li>
+              </ul>
             </div>
         </div>
       </div>
