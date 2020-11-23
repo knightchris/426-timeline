3939b00bebbfc7b32da64ab984cda4bd21488ec2
Anthony M <ajamesmichaels@gmail.com>
2020-11-22 20:37:34 -0500

    Adds caps to timeline mediatypes

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -81,7 +81,7 @@ function Sidebar(props) {
           /> 
             {/* <button className="sidenav-button">Filter</button> */}
             <div>
-              <label for="dropdown" id="sort-dropdown-label">Sort by:</label>
+              <label htmlFor="dropdown" id="sort-dropdown-label">Sort by:</label>
               <br></br>
               <select name="dropdown" id="dropdown">
                 <option className="sort-option" value="Published Date, Ascending">Published Date, Ascending</option>
@@ -89,8 +89,8 @@ function Sidebar(props) {
                 <option className="sort-option" value="Universe Date, Ascending">Universe Date, Ascending</option>
                 <option className="sort-option" value="Universe Date, Descending">Universe Date, Descending</option>
               </select>
-              <svg onClick={handleSort} id="sort-go" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right" fill="white" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+              <svg onClick={handleSort} id="sort-go" width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-right" fill="white" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
               </svg>
             </div>
         </div>
